9b5a7bc7eb59d10b9384ec74aec8530dd63cd84b
Alireza <alirezammww@gmail.com>
2025-01-13 17:25:21 +0330

    added new files

---
+++ src/components/modules/navbar/Navbar.js
@@ -0,0 +1,82 @@
+import styles from "./navbar.module.css";
+import { Container } from "react-bootstrap";
+import Link from "next/link";
+import Image from "next/image";
+import logoImage from "@/public/images/logo_svg.svg";
+import { FiSearch } from "react-icons/fi";
+import { FaUser } from "react-icons/fa";
+import { IoHeart } from "react-icons/io5";
+import { RiShoppingBasketFill } from "react-icons/ri";
+
+export default function Navbar() {
+  return (
+    <div className={styles.navbarContainer}>
+      <Container>
+        <div className={styles.navbar}>
+          <div className={styles.logoBox}>
+            <Image src={logoImage} alt="logo" />
+          </div>
+
+          <div className={styles.menuBox}>
+            <ul className={styles.menu}>
+              <li className={styles.menuItem}>
+                <Link href="/" className={styles.menuLink}>
+                  خانه
+                </Link>
+              </li>
+              <li className={styles.menuItem}>
+                <Link href="/" className={styles.menuLink}>
+                  منو
+                </Link>
+              </li>
+              <li className={styles.menuItem}>
+                <Link href="/" className={styles.menuLink}>
+                  درباره ما
+                </Link>
+              </li>
+              <li className={styles.menuItem}>
+                <Link href="/" className={styles.menuLink}>
+                  فروشگاه
+                </Link>
+              </li>
+              <li className={styles.menuItem}>
+                <Link href="/" className={styles.menuLink}>
+                  وبلاگ ها
+                </Link>
+              </li>
+              <li className={styles.menuItem}>
+                <Link href="/" className={styles.menuLink}>
+                  ارتباط با ما
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.iconsBox}>
+            <div className={styles.iconBox}>
+              <FiSearch className={styles.icon} />
+            </div>
+
+            <div className={styles.iconBox}>
+              <FaUser className={styles.icon} />
+            </div>
+
+            <div className={styles.iconBox}>
+              <Link href="/">
+                <IoHeart className={styles.icon} />
+                <span className={styles.wishlistCount}>0</span>
+              </Link>
+            </div>
+
+            <div className={styles.iconBox}>
+              <Link href="/">
+                <RiShoppingBasketFill className={styles.icon} />
+                <span className={styles.shoppingCount}>0</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
---
+++ src/components/templates/index/landing/Landing.js
@@ -0,0 +1,118 @@
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./landing.module.css";
+import { Row, Col } from "react-bootstrap";
+import Image from "next/image";
+import Link from "next/link";
+import pizzaImage from "@/public/images/pizza.png";
+import panirImage from "@/public/images/khordePanir.png";
+import tomatoImage from "@/public/images/tomato.png";
+import gharchImage from "@/public/images/gharch.png";
+import reyhoonImage from "@/public/images/reyhoon.png";
+import bargImage from "@/public/images/barg.png";
+
+export default function Landing() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  // ذخیره موقعیت موس
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  // ذخیره موقعیت اسکرول
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setScrollPosition(scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // محاسبه استایل مربوط به حرکت موس
+  const parallaxStyle = (factor) => ({
+    transform: `translate(${mousePosition.x * factor}px, ${
+      mousePosition.y * factor
+    }px)`,
+  });
+
+  // محاسبه استایل مربوط به اسکرول
+  const scrollStyle = (factor) => ({
+    transform: `translateY(${scrollPosition * factor}px)`,
+    transition: "transform 0.1s ease-out",
+  });
+
+
+  return (
+    <div className={styles.landingContainer}>
+      <Image
+        src={tomatoImage}
+        alt="Tomato"
+        className={`${styles.decorImage} ${styles.tomatoImage}`}
+        style={parallaxStyle(-0.01)}
+      />
+      <Image
+        src={panirImage}
+        alt="Mushroom"
+        className={`${styles.decorImage} ${styles.panirImage}`}
+        style={scrollStyle(0.15)}
+      />
+      <Image
+        src={gharchImage}
+        alt="Herb"
+        className={`${styles.decorImage} ${styles.gharchImage}`}
+        style={scrollStyle(0.2)}
+      />
+      <Image
+        src={reyhoonImage}
+        alt="Herb"
+        className={`${styles.decorImage} ${styles.reyhoonImage}`}
+        style={parallaxStyle(0.01)}
+      />
+      <Image
+        src={bargImage}
+        alt="Herb"
+        className={`${styles.decorImage} ${styles.bargImage}`}
+        style={scrollStyle(0.2)}
+      />
+
+      <Row className={styles.row}>
+        <Col md={6} sm={12}>
+          <div className={styles.rightSide}>
+            <h1 className={styles.title}>
+              اینجا هر <span className={styles.pizzaWord}>پیتزا</span> داستانی
+              دارد!
+            </h1>
+            <button className={styles.orderBtn}>
+              <Link href="/" className={styles.orderBtnLink}>
+                اکنون سفارش دهید
+              </Link>
+            </button>
+          </div>
+        </Col>
+
+        <Col md={6} sm={12}>
+          <div className={styles.leftSide}>
+            <Image
+              src={pizzaImage}
+              alt="Pizza Image"
+              className={styles.pizzaImage}
+              style={scrollStyle(0.15)}
+            />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+}
